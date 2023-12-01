9c6df8febd01222b23ca2f557defad5f38e2b864
Ngathet2522000 <thetnaingaungDeveloper@gmail.com>
2023-12-01 12:09:44 +0630

    Api-Setup-And-Component-Extraction

---
+++ src/components/ClearBtn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ClearBtn() {
+    return (
+        <div>
+            <button className="button">Clear completed</button>
+        </div>
+    )
+}
